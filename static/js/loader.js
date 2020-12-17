2bd623f546f6dcc31437d6953c2a0324ac0e7708
Joshua inyang <joshuainyang255@gmail.com>
2020-12-17 15:22:11 +0100

    preloader

--- static/js/loader.js
+++ static/js/loader.js
@@ -28,3 +28,12 @@ $(document).ready(function () {
     
     
 });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add
+  }
+})
+
