c7438010785770c560af9d3962a5586dacba52bf
Joshua inyang <joshuainyang255@gmail.com>
2020-12-21 00:17:31 +0100

    darkmode commit

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,13 +1,13 @@
 const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
+    bottom: '30px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: false, // default: true,
+    saveInCookies: true, // default: true,
     label: '🌓', // default: ''
     autoMatchOsTheme: true // default: true
 }
